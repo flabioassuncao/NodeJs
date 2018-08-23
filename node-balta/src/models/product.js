9f8b7e5881ca1dda8839cb303bce7742c2169778
Flábio Assunção <flabiofilho@gmail.com>
2018-08-23 00:46:56 -0300

    ate aula 15

---
+++ node-balta/src/models/product.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const moongose = require('mongoose') ;
+const Schema = moongose.Schema;
+
+const schema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    slug: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+        unique: true
+    },
+    description : {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    tags: [{
+        type: String,
+        required: true
+    }]
+});
+
+mondule.exports = moongose.model('Product', schema);
