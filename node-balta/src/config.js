8a769572b6dbdd41fe397ffa85c9339819b2bcde
Flábio Assunção <flabiofilho@gmail.com>
2018-08-28 08:19:30 -0300

    Update config.js

--- node-balta/src/config.js
+++ node-balta/src/config.js
@@ -1,8 +1,2 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
-
-module.exports = {
-    connectionString: 'mongodb://flabio10:flabio10@ds020168.mlab.com:20168/nodecourse',
-    sendgridKey: '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',
-    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=coursenode;AccountKey=foPDWPVru+3JqHgtJ0SaDrN20HmqdDEz9spxwBuTK195acdf8xiCOBKAsLLWcruDaYjuavrwDzhRnFeqRAidMQ==;EndpointSuffix=core.windows.net'
-}
